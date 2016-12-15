2f16ceda38d6a95a45dd3bc9f6e8f4dbd68d19c5
hoangphucvu <ngohungphuc95@gmail.com>
2016-12-15 13:50:18 +0700

    Modal dialog

--- src/app/app.js
+++ src/app/app.js
@@ -1,26 +1,27 @@
 (function () {
-   'use strict';
+    'use strict';
 
-   // Create the module and define its dependencies.
-   var app = angular.module('app', [
-      // Angular modules
-      'ngRoute',           // routing
+    // Create the module and define its dependencies.
+    var app = angular.module('app', [
+        // Angular modules
+        'ngRoute',           // routing
 
-      // Custom modules
+        // Custom modules
 
-      // 3rd Party Modules
-      'kendo.directives'
-   ]);
+        // 3rd Party Modules
+        'kendo.directives'
+    ]);
 
-   // config always runs before the services are ready.
-   // basically the first thing our module does.
-   app.config(['$routeProvider', function($routeProvider) {
-      $routeProvider
-         .when('/', { templateUrl: 'app/welcome/welcome.html' })
-          .when('/welcome2', { templateUrl: 'app/welcome/welcome2.html' })
-          .when('/welcome3', { templateUrl: 'app/welcome/welcome3.html' })
-          .otherwise({ redirectTo: '/' }); // go to the welcome page
-   }
-   ]);
+    // config always runs before the services are ready.
+    // basically the first thing our module does.
+    app.config(['$routeProvider', function ($routeProvider) {
+        $routeProvider
+            .when('/', {templateUrl: 'app/welcome/welcome.html'})
+            .when('/welcome2', {templateUrl: 'app/welcome/welcome2.html'})
+            .when('/welcome3', {templateUrl: 'app/welcome/welcome3.html'})
+            .when('/modal', {templateUrl: 'app/modal/modal.html'})
+            .otherwise({redirectTo: '/'}); // go to the welcome page
+    }
+    ]);
 
 })();
